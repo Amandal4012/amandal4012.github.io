cd07389895a5555233dc00b0663367700c698cfa
coco <43858608+Amandal4012@users.noreply.github.com>
2019-10-07 09:37:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Amanda"
+let userName = "Amandal4012";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
